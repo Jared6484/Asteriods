1bd2a8cc56c5a77eb7d8b09b9c6c9a824ec84d81
jared6484 <jared.6484@gmail.com>
2025-07-29 00:13:14 -0400

    B: Player can move left and right

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ def main():
         player.draw(screen)
         #pygame.display.flip()
         dt = clock.tick(60)/1000
+        player.update(dt)
         pygame.display.update()
 
     pygame.quit()
